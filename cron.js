ba8338bfd4761f8672d46619b5d16745c432771d
bhushan <bhushan.j.shinde@gmail.com>
2023-08-21 12:18:48 +0800

    Solution

--- cron.js
+++ cron.js
@@ -12,13 +12,47 @@ const appsRepository = require("./repositories/apps-repository");
  * if the is_live status calculated from this function is different than what is currently saved in the database, the new status needs to be saved in the db by using the repository function:
  * appsRepository.saveApp(app)
  *
- *
- * @param sites
  */
-function calculateIsLive() {
-  const campaigns = campaignRepository.getAllCampaigns();
+
+async function calculateIsLive() {
+  const campaigns = await campaignRepository.getAllCampaigns();
   const apps = appsRepository.getAllApps();
-  // write your code here...
+
+  let liveReasons = {}; // To store the reason for each app
+
+  let appUrlToIdMap = {};
+  for (let app of apps) {
+    appUrlToIdMap[app.url] = app.id;
+  }
+
+  // Check each campaign to see if it makes an app live.
+  for (let campaign of campaigns) {
+    if (campaign.status === 'running') {
+      let appId = appUrlToIdMap[campaign.redirect_url];
+      if (appId) {
+        liveReasons[appId] = `App ${appId} is LIVE because campaign ${campaign.campaign_id} links to it and is running.`;
+      }
+    }
+  }
+
+  // Check each app's links to see if they make any other apps live.
+  for (let app of apps) {
+    if (liveReasons[app.id]) {
+      for (let link of app.links) {
+        let linkedAppId = appUrlToIdMap[link];
+        if (linkedAppId && !liveReasons[linkedAppId]) {
+          liveReasons[linkedAppId] = `App ${linkedAppId} is LIVE because App ${app.id} is live and links to App ${linkedAppId}`;
+        }
+      }
+    }
+  }
+
+  // Print for each app from the first app. ToDo - Save object
+  for (let app of apps) {
+    console.log(liveReasons[app.id] || `App ${app.id} is NOT LIVE`);
+  }
 }
 
 calculateIsLive();
+
+//ToDo - logs only onSave or log while traversing
--- index.js
+++ index.js
@@ -5,13 +5,32 @@ const campaignRepository = require("./repositories/campaign-repository");
 const app = express();
 const port = 4000;
 
-app.get("/scan/:codeId", (req, res) => {
+app.get("/scan/:codeId", async (req, res) => {
   const codeId = req.params.codeId;
 
-  // WRITE YOUR CODE HERE
+  try {
+    const qrCode = await qrCodeRepository.getCodeById(codeId);
+    if (!qrCode) {
+      return res.status(404).send("QR Code not found or available.");
+    }
+    const campaignId = await campaignRollsRepository.getCampaignIdByRollId(qrCode.roll_id);
 
-  // your redirect URL goes here
-  res.redirect("<your_redirect_URL>");
+    if (!campaignId) {
+      return res.status(404).send("Campaign not found for the QR code.");
+    }
+
+    const campaign = await campaignRepository.getCampaignById(campaignId);
+
+    if (campaign && campaign.status === "running") {
+      res.redirect(campaign.redirect_url);
+    } else {
+      res.redirect(campaign.default_url);
+    }
+
+  } catch (error) {
+    console.error("Error scanning QR Code:", error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 app.listen(port, () => {
